a1c9c20fc3f1db3174f123fced69b14a7b3a68ab
Eriife-git <eriifefashonista22@gmail.com>
2025-05-02 10:00:07 +0100

    MortgageCalculator.java

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -5,7 +5,7 @@ public class MortgageCalculator {
 	Scanner inputCollector = new Scanner(System.in);
 
 	System.out.print("Enter the principal amount: ");
-	double principal = inputCollector.nextDouble();
+	double principalAnount = inputCollector.nextDouble();
 
 	System.out.print("Enter the annual interest rate: ");
 	double annualInterestRate = inputCollector.nextDouble();
@@ -16,11 +16,11 @@ public class MortgageCalculator {
 	double monthlyInterestRate = (annualInterestRate / 100) / 12;
 	double numberOfMonth = numberOfYears * 12;
 
-	double firstFormula = monthlyInterestRate * Math.pow((1 + monthlyInterestRate), numberOfMonth);
+	double paymentCal = monthlyInterestRate * Math.pow((1 + monthlyInterestRate), numberOfMonth);
 	
-	double secondFormula = Math.pow((1 + monthlyInterestRate), numberOfMonth) - 1;
+	double paymentCal2 = Math.pow((1 + monthlyInterestRate), numberOfMonth) - 1;
 
-	double mortgagePayment = principal * firstFormula / secondFormula;
+	double mortgagePayment = principalAmount * paymentCal / paymentCal2;
 
 	System.out.printf("Your monthly payment is $%.2f", mortgagePayment); 
 	
